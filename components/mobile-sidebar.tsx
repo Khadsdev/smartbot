dfb5256af89c0240e4832ea3beffe0e13159945f
Khadsdev <36850593+Khadsdev@users.noreply.github.com>
2024-09-01 00:18:54 +0200

    made the navbar and mobile navbar

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
+
+const MobileSideBar = () => {
+  //fix components which are causing hidration error
+  // by the way they are rendered
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []); // <-- []empty dependency array
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant='ghost' size='icon' className='md:hidden'>
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side='left' className='p-0'>
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSideBar;
